d64e0ec0b2001fe4b020e24d3c52f0e5a35db12d
AnasNaamneh <naamnih.anas@gmail.com>
2024-02-03 01:53:12 -0800

    Prepare Connection String,GetAllCustomers,GetCustomerInfoByID

---
+++ frmTest.cs
@@ -0,0 +1,30 @@
+﻿using OnlineStoreBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineStoreWinform
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          //  dataGridView1.DataSource = clsCustomer.GetAllCustomer();
+
+            clsCustomer customer = clsCustomer.Find(1);
+
+            MessageBox.Show(customer.Email);
+        }
+    }
+}
